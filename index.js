f02425fb514241f65f3cb28b784c545a9114b5cc
Jake Runzer <jakerunzer@gmail.com>
2016-06-07 18:32:34 -0700

    tweets

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 const Emoji = require('./emoji.js');
+const Secrets = require('./secrets.js');
+const Twitter = require('twitter');
 
 const emojiFile = 'emoji-list.json';
 
-const e = Emoji.freshEmoji(emojiFile);
+const client = new Twitter({
+    consumer_key: Secrets.consumerKey,
+    consumer_secret: Secrets.consumerSecret,
+    access_token_key: Secrets.accessTokenKey,
+    access_token_secret: Secrets.accessTokenSecret
+});
+
+let e = '';
+
+let tweetEmoji = function() {
+    e = Emoji.freshEmoji(emojiFile);
+
+    client.post('statuses/update', {
+        status: e
+    }, function(error, tweet, response) {
+        if (error) {
+            console.log(error);
+        }
+    });
+}
+
+tweetEmoji();
